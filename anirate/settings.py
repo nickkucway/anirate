880dd8e638b046f1b81e8aaacc855a01022b413a
Nickkucway <nickkucway@gmail.com>
2024-02-05 10:47:10 -0600

    added ability to create new user and form

--- anirate/settings.py
+++ anirate/settings.py
@@ -120,6 +120,8 @@ STATIC_URL = 'static/'
 
 # redirect user on login
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
